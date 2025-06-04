e8c4ebdaae7e3745463582cd0b26f4f3b19b2150
Rulian14 <Rulian1403@gmail.com>
2025-06-04 20:49:32 -0300

    Troquei o titulo da pagina

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -3,7 +3,7 @@ import "../assets/HeaderCss.css";
 function Header() {
   return (
     <div className="HeaderArea">
-      <h1>Analizador</h1>
+      <h1>Statix</h1>
       <button className="BotaoHeader">menu</button>
     </div>
   );
