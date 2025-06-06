15ed2878892bef5a6b4fa3d4ff4ac84b466e313f
GustavoKloster <gukjorge2003@gmail.com>
2025-06-05 22:07:59 -0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -7,16 +7,13 @@ import "./assets/App.css";
 
 function App() {
   const [texto, setTexto] = useState("");
+  const [palavras, setPalavras] = useState([]); // <- novo estado
 
-  const palavras = texto
-    .split(/[\s,.;:\n\t]+/)
-    .map((p) => p.trim())
-    .filter((p) => /^[A-Za-zÀ-ÿ]+$/.test(p));
   return (
     <div>
       <Cabecalho />
       <main>
-        <Receber texto={texto} setTexto={setTexto} />
+        <Receber texto={texto} setTexto={setTexto} setPalavras={setPalavras} />
         <PrintNumeros texto={texto} />
         <CalcularEstatisticasStrings texto={palavras} />
       </main>
